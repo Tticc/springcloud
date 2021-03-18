1da166b505fe10c24acc6fa1ff1a381f6ad0c8eb
Tticc <18883394480@163.com>
2021-03-18 08:01:52 +0800

    add 、mq

---
+++ cloud-rocketmq/src/main/java/com/wenc/springcloud/rocketmq/CloudRocketMQApplication.java
@@ -0,0 +1,14 @@
+package com.wenc.springcloud.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 2021-3-9 21:23:38
+ */
+@SpringBootApplication
+public class CloudRocketMQApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudRocketMQApplication.class, args);
+    }
+}
